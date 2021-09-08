57f8460e33b79305fc3e1fdb43576ba9ff4a1609
Robin Lange <robin.langenc@gmail.com>
2021-09-08 21:38:18 +1000

    Cronline: do not auto-fill fixed value if there's at least one "every"
    Fixes some weird logic with the time parsing

--- src/agenda/time_parsing/cronline_builder.rs
+++ src/agenda/time_parsing/cronline_builder.rs
@@ -31,50 +31,56 @@ impl CronlineBuilder {
 
         debug!("Autofilling cronline: {:?}", self.map);
     
-        // Auto-filling wildcards (a.k.a "CronValue::Every") columns
-        debug!("Filling wildcards");
-        let mut wildcard_fill_state = false;
-        for col in CRON_COLUMNS.iter() {
 
-            debug!("Column {:?}, wildcard={}", col, wildcard_fill_state);
+        let has_wildcard = {
+            // Auto-filling wildcards (a.k.a "CronValue::Every") columns
+            debug!("Filling wildcards");
+            let mut wildcard_fill_state = false;
+            for col in CRON_COLUMNS.iter() {
+    
+                debug!("Column {:?}, wildcard={}", col, wildcard_fill_state);
+    
+                match self.map.get(&col).copied() {
+        
+                    None if wildcard_fill_state => { self.map.insert(*col, CronValue::Every); },
+                    None => (),
+        
+                    Some(CronValue::Every) => {
+                        wildcard_fill_state = true;
+                        self.map.insert(*col, CronValue::Every);
+                    },
+        
+                    Some(CronValue::On(_val)) if wildcard_fill_state => break,
+                    Some(CronValue::On(val)) => { self.map.insert(*col, CronValue::On(val)); },
+                };
+            }
 
-            match self.map.get(&col).copied() {
-    
-                None if wildcard_fill_state => { self.map.insert(*col, CronValue::Every); },
-                None => (),
-    
-                Some(CronValue::Every) => {
-                    wildcard_fill_state = true;
-                    self.map.insert(*col, CronValue::Every);
-                },
-    
-                Some(CronValue::On(_val)) if wildcard_fill_state => break,
-                Some(CronValue::On(val)) => { self.map.insert(*col, CronValue::On(val)); },
-            };
-        }
-    
+            wildcard_fill_state
+        };
 
-        // Auto-filling fixed columns
-        debug!("Filling fixed columns");
-        let cronline_now = Cronline::from_time(now);
-        let mut fixed_fill_state = false;
-        for col in CRON_COLUMNS.iter() {
+        if !has_wildcard {
+            // Auto-filling fixed columns
+            debug!("Filling fixed columns");
+            let cronline_now = Cronline::from_time(now);
+            let mut fixed_fill_state = false;
+            for col in CRON_COLUMNS.iter() {
 
-            debug!("Column {:?}, fixed={}", col, fixed_fill_state);
-            
-            match self.map.get(&col).copied() {
-    
-                None if fixed_fill_state => { self.map.insert(*col, cronline_now.get(*col)); },
-                None => (),
-    
-                Some(CronValue::Every) if fixed_fill_state => break,
-                Some(CronValue::Every) => { self.map.insert(*col, CronValue::Every); },
-    
-                Some(CronValue::On(val)) => {
-                    fixed_fill_state = true;
-                    self.map.insert(*col, CronValue::On(val));
-                }
-            };
+                debug!("Column {:?}, fixed={}", col, fixed_fill_state);
+                
+                match self.map.get(&col).copied() {
+        
+                    None if fixed_fill_state => { self.map.insert(*col, cronline_now.get(*col)); },
+                    None => (),
+        
+                    Some(CronValue::Every) if fixed_fill_state => break,
+                    Some(CronValue::Every) => { self.map.insert(*col, CronValue::Every); },
+        
+                    Some(CronValue::On(val)) => {
+                        fixed_fill_state = true;
+                        self.map.insert(*col, CronValue::On(val));
+                    }
+                };
+            }
         }
 
         let mut comment = None;
