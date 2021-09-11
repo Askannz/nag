c7edfab517eb0c678e3bf54ca19cea23b579bba3
Robin Lange <robin.langenc@gmail.com>
2021-09-11 13:25:45 +1000

    Set logging verbosity in CLI options

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,11 @@ use http::HTTP_Notifier;
 
 fn main() {
 
-    env_logger::Builder::new()
-        .filter_module("nag", log::LevelFilter::Debug)
-        .parse_default_env()
-        .init();
-
     let opts = Opts::parse();
+
+    env_logger::Builder::new()
+        .filter_module("nag", opts.verbosity.to_level())
+        .init();
 
     let (sender, receiver) = unbounded();
 
@@ -67,7 +66,10 @@ pub struct Opts {
     endpoint_host: String,
 
     #[clap(long, parse(try_from_str), default_value="8123")]
-    endpoint_port: u16
+    endpoint_port: u16,
+
+    #[clap(long, short, arg_enum, default_value="info")]
+    verbosity: Verbosity
 }
 
 #[derive(ArgEnum, Clap, Clone, Debug)]
@@ -83,6 +85,40 @@ impl FromStr for DateFormat {
             "dmy" => Ok(Self::DMY),
             "mdy" => Ok(Self::MDY),
             _ => anyhow::bail!("Cannot parse {}", s)
+        }
+    }
+}
+
+#[derive(ArgEnum, Clap, Clone, Debug)]
+enum Verbosity {
+    Off, Trace, Debug, Info, Warn, Error
+}
+
+impl FromStr for Verbosity {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let v = match s.to_lowercase().as_str() {
+            "off"   => Self::Off,
+            "trace" => Self::Trace,
+            "debug" => Self::Debug,
+            "info"  => Self::Info,
+            "warn"  => Self::Warn,
+            "error" => Self::Error,
+            _ => anyhow::bail!("Cannot parse {}", s)
+        };
+        Ok(v)
+    }
+}
+
+impl Verbosity {
+    fn to_level(&self) -> log::LevelFilter {
+        match self {
+            Self::Off   => log::LevelFilter::Off,
+            Self::Trace => log::LevelFilter::Trace,
+            Self::Debug => log::LevelFilter::Debug,
+            Self::Info  => log::LevelFilter::Info,
+            Self::Warn  => log::LevelFilter::Warn,
+            Self::Error => log::LevelFilter::Error
         }
     }
 }
